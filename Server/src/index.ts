c5679345613021254d9ae60e5ad5f3ba4ba1862b
ArunavaCoderEm <acecankill@gmail.com>
2024-06-15 23:21:15 +0530

    hono push

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,16 +1,20 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hono Server Started at localhost:8173')
-})
+const port : number | undefined = (process.env.PRODUCTION === 'production') ? Number(process.env.PRODSERVE) : Number (process.env.PORT)
 
-const port = 8173
 console.log(`Server is running on port ${port}`)
+
+app.get('/', (c) => {
+  return c.text(`Hono Server Started at localhost:${port}`)
+})
 
 serve({
   fetch: app.fetch,
-  port
+  port 
 })
