f5198ed20cf782d4237411fd7f93ba56f16fdfad
ArunavaCoderEm <acecankill@gmail.com>
2024-06-24 21:55:27 +0530

    Booking INIT

---
+++ Server/src/Bookings/Bookingcl.ts
@@ -0,0 +1,5 @@
+import { Hono } from "hono";
+import modelSchemaexpuser from '../Schema/Clientschema'
+import modelSchemaexpprov from "../Schema/Roledataschema";
+
+const servercl = new Hono();
---
+++ Server/src/Bookings/Bookingpr.ts
@@ -0,0 +1,6 @@
+import { Hono } from "hono";
+import modelSchemaexpuser from '../Schema/Clientschema'
+import modelSchemaexpprov from "../Schema/Roledataschema";
+
+const servercl = new Hono();
+
