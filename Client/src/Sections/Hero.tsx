10c3dd22ac4b6412dfc4c0e49b252b65ecef426a
ArunavaCoderEm <acecankill@gmail.com>
2024-07-07 23:48:08 +0530

    okay shit's not for toda ! bye tom i';ll see

--- Client/src/Sections/Hero.tsx
+++ Client/src/Sections/Hero.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Hero():React.ReactNode {
   return (
-    <div>Hero</div>
+    <>
+    <h1 className='text-center mt-10 lg:text-5xl text-2xl bg-clip-text text-transparent bg-gradient-to-b from-gray-800 to-gray-500 font-extrabold m-3 p-3'>Vehycle : Find Reliable <span className='text-black'>Mechanics Near You</span> - Your Trusted Partner for All Vehicle Needs.</h1>
+    <div className='grid lg:grid-cols-2 p-3 sm:grid-cols-1'>
+        <div className='bg-red-300 flex flex-col items-center justify-center'>
+
+        </div>
+        <div className='bg-red-600'>f</div>
+    </div>
+    </>
   )
 }
