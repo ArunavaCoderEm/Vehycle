4f5fbb1d0629723d3f03b4eaa91ea46676081607
ArunavaCoderEm <acecankill@gmail.com>
2024-07-08 23:40:25 +0530

    okay nice

--- Client/src/Components/Imgfade.tsx
+++ Client/src/Components/Imgfade.tsx
@@ -3,13 +3,13 @@ import { FC } from 'react'
 const ImageFade: FC = (): React.ReactNode => {
   return (
     <div>
-      <div className="relative flex max-w-6xl justify-center overflow-hidden">
+      <div className="relative rounded-lg flex max-w-6xl justify-center overflow-hidden">
         <img
           src="./heropicjpg.jpeg"
           alt="Heropic"
           className="h-[28rem] sha w-96 rounded-lg object-cover md:w-[1300px]"
           style={{
-            maskImage: `linear-gradient(to top, transparent, black 20%)`,
+            maskImage: `linear-gradient(to top, transparent, black 30%)`,
           }}
         />
       </div>
--- Client/src/Sections/Hero.tsx
+++ Client/src/Sections/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero():React.ReactNode {
     <>
     <h1 className='text-center mt-20 lg:text-5xl text-2xl bg-clip-text text-transparent bg-gradient-to-b from-gray-800 to-gray-500 font-extrabold m-3 p-3'>Vehycle : Find Reliable <span className='text-black'>Mechanics Near You</span> - Your Trusted Partner for All Vehicle Needs.</h1>
     <div className='grid lg:grid-cols-2 p-3 sm:grid-cols-1'>
-        <div className='  p-2 flex flex-col items-center justify-center'>
+        <div className='  p-2 flex flex-col rounded-lg items-center justify-center'>
           <ImageFade />
         </div>
         <div className='p-2 flex flex-col justify-center items-center'>
