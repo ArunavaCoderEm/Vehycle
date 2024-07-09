a8be4ff44c6bccb789e0b7a747be11688c88b213
ArunavaCoderEm <acecankill@gmail.com>
2024-07-09 21:46:38 +0530

    initial about pushes

---
+++ Client/src/Sections/About.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function About():React.ReactNode {
+  return (
+    <>
+    <div>
+        <h1 className='text-4xl text-center my-5 font-extrabold'><span className='text-pink-600'>A</span>bout <span className='text-pink-600'>V</span>ehycle</h1>
+        <div className='grid p-2 lg:grid-cols-3 gap-4 sm:grid-cols-1'>
+            <div className='bg-gradient-to-b from-black/70 rounded-lg to-black/40'>
+            s
+            </div>
+            <div className='bg-gradient-to-b from-black/70 rounded-lg to-black/40'>
+            
+            </div>
+            <div className='bg-gradient-to-b from-black/70 rounded-lg to-black/40'>
+            
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
