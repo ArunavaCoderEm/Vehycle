89b6e6e0e9ad9894dabf2239581afdb7e12ae3cd
ArunavaCoderEm <acecankill@gmail.com>
2024-07-28 00:32:27 +0530

    Backend updated schema and routes

--- Server/src/Routes/SearSorSup.ts
+++ Server/src/Routes/SearSorSup.ts
@@ -6,7 +6,7 @@ const serverseasorsp = new Hono();
 serverseasorsp.get("/getclient/:id", async (c) => {
     try {
         const par = c.req.param('id')
-        const res = await modelSchemaexpuser.find({ specialist : par })
+        const res = await modelSchemaexpuser.find({ current_defect : par })
         if(!res.length){
             console.log(`Document doesn't exist`);
             return c.json(
