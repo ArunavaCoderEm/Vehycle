de10c882269d133d86e716c13a6d0bab4d199c0b
ArunavaCoderEm <acecankill@gmail.com>
2024-07-29 19:35:52 +0530

    dummy ppush

--- Client/src/Components/Alert.tsx
+++ Client/src/Components/Alert.tsx
@@ -6,7 +6,7 @@ export default function Alert(props:any):React.ReactNode {
     <div className='mt-20'>
     <div className="bg-pink-100 border w-96 mx-auto z-[42] border-pink-400 text-pink-500 px-4 py-3 rounded relative" role="alert">
     <strong className="font-bold">{props.title}</strong>
-    <span className="block sm:inline mx-10">{props.descr}</span>
+    <span className="block sm:inline mx-10">{props.descr}</span> 
     </div>
     </div>
     </>
